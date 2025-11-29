138303937978c2a7788156cdfd6ad8a83ae923d6
achyutkneupane <achyutkneupane@gmail.com>
2025-11-30 01:21:24 +0545

    feat: adds BlogCategory resource with management capabilities

---
+++ app/Filament/Resources/BlogCategories/BlogCategoryResource.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\BlogCategories;
+
+use App\Filament\Resources\BlogCategories\Pages\ManageBlogCategories;
+use App\Models\BlogCategory;
+use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use UnitEnum;
+
+class BlogCategoryResource extends Resource
+{
+    protected static ?string $model = BlogCategory::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::RectangleStack;
+
+    protected static string|BackedEnum|null $activeNavigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static string|UnitEnum|null $navigationGroup = 'Blog Management';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->columns(1)
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('slug')
+                    ->visibleOn('edit')
+                    ->required(),
+            ]);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->columns(1)
+            ->components([
+                TextEntry::make('name')
+                    ->inlineLabel(),
+                TextEntry::make('slug')
+                    ->inlineLabel(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageBlogCategories::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/BlogCategories/Pages/ManageBlogCategories.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\BlogCategories\Pages;
+
+use App\Filament\Resources\BlogCategories\BlogCategoryResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageBlogCategories extends ManageRecords
+{
+    protected static string $resource = BlogCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
