6a0d9367187fbedd4eecf7848ce05bf6ba034606
achyutkneupane <achyutkneupane@gmail.com>
2025-11-29 12:05:28 +0545

    feat: updates UserRole enum Includes new Writer role with associated label and icon.

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
+use BackedEnum;
 use Filament\Support\Colors\Color;
 use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Contracts\Support\Htmlable;
 
-enum UserRole: string implements HasColor, HasLabel
+enum UserRole: string implements HasColor, HasIcon, HasLabel
 {
     case Developer = 'developer';
     case Admin = 'admin';
+    case Writer = 'writer';
     case User = 'user';
 
     public function getLabel(): ?string
@@ -19,6 +24,7 @@ enum UserRole: string implements HasColor, HasLabel
         return match ($this) {
             self::Developer => 'Developer',
             self::Admin => 'Admin',
+            self::Writer => 'Writer',
             self::User => 'User',
         };
     }
@@ -28,7 +34,18 @@ enum UserRole: string implements HasColor, HasLabel
         return match ($this) {
             self::Developer => Color::Red,
             self::Admin => Color::Blue,
+            self::Writer => Color::Yellow,
             self::User => Color::Green,
+        };
+    }
+
+    public function getIcon(): string|BackedEnum|Htmlable|null
+    {
+        return match ($this) {
+            self::Developer => Heroicon::CodeBracket,
+            self::Admin => Heroicon::ShieldCheck,
+            self::Writer => Heroicon::PencilSquare,
+            self::User => Heroicon::UserCircle,
         };
     }
 }
