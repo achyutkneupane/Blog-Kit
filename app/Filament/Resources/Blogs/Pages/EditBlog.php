f38a6e8a57702a4e3d04310a051cdf8223a5f3a2
achyutkneupane <achyutkneupane@gmail.com>
2025-12-01 00:13:15 +0545

    feat: adds publish scheduling logic in EditBlog form Implements logic to determine the publish status based on the published_at date.

--- app/Filament/Resources/Blogs/Pages/EditBlog.php
+++ app/Filament/Resources/Blogs/Pages/EditBlog.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Blogs\Pages;
 
+use App\Enums\ScheduleOption;
 use App\Filament\Resources\Blogs\BlogResource;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
@@ -12,6 +13,20 @@ use Filament\Resources\Pages\EditRecord;
 class EditBlog extends EditRecord
 {
     protected static string $resource = BlogResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $publishedAt = $data['published_at'];
+        if ($publishedAt === null) {
+            $data['publish'] = ScheduleOption::Draft;
+        } elseif ($publishedAt > now()) {
+            $data['publish'] = ScheduleOption::Later;
+        } else {
+            $data['publish'] = ScheduleOption::Now;
+        }
+
+        return $data;
+    }
 
     protected function getHeaderActions(): array
     {
