88fd30f3e332a5df235a24fa2c035ce4f8290580
achyutkneupane <achyutkneupane@gmail.com>
2025-12-02 22:27:45 +0545

    refactor: removes unused imports from ExampleTest and FeatureTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Enums\UserRole;
-use App\Models\User;
-
 it('returns a redirection response', function (): void {
     $response = $this->get('/admin');
 
--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Enums\UserRole;
-use App\Models\User;
-
-use function Pest\Laravel\actingAs;
-
 it('redirects to login page when filament is requested being guest', function (): void {
     $response = $this->get('/admin');
 
