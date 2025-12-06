ef8a62b7059a5a2a58bd7234de5989a2b5d7d4fb
achyutkneupane <achyutkneupane@gmail.com>
2025-12-06 18:19:13 +0545

    feat: updates hero section layout for improved responsiveness

--- resources/views/components/section/hero-section.blade.php
+++ resources/views/components/section/hero-section.blade.php
@@ -1,5 +1,5 @@
-<section class="grid lg:gap-8 xl:gap-0 lg:grid-cols-12 py-6 lg:py-12">
-    <div class="mr-auto place-self-center lg:col-span-7">
+<section class="grid gap-8 lg:grid-cols-12 py-6 lg:py-12 px-4 lg:px-0">
+    <div class="mr-auto place-self-center md:col-span-7 order-2 lg:order-1">
         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold md:text-5xl xl:text-6xl text-primary">
             Laravel Blog Kit
         </h1>
@@ -11,7 +11,7 @@
             <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </a>
     </div>
-    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+    <div class="lg:mt-0 md:col-span-5 flex order-1 lg:order-2">
         <img src="https://hamrocdn.com/mhykONU_Vi2F" alt="Screenshot Image" class="rounded-base border-2 border-primary-200 shadow-sm shadow-primary" />
     </div>
 </section>
