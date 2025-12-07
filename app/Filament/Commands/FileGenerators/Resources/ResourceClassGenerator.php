d2e5ccfb62dd1b70e54f13e90837259a1e082b3e
achyutkneupane <achyutkneupane@gmail.com>
2025-12-07 22:20:48 +0545

    feat: adds subNavigationPosition and getRecordSubNavigation method

--- app/Filament/Commands/FileGenerators/Resources/ResourceClassGenerator.php
+++ app/Filament/Commands/FileGenerators/Resources/ResourceClassGenerator.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Filament\Commands\FileGenerators\Resources;
 
 use Filament\Commands\FileGenerators\Resources\ResourceClassGenerator as BaseResourceClassGenerator;
+use Filament\Pages\Enums\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Nette\PhpGenerator\ClassType;
 use Nette\PhpGenerator\Literal;
 
@@ -27,5 +29,55 @@ class ResourceClassGenerator extends BaseResourceClassGenerator
             ->setStatic()
             ->setType('string|BackedEnum|null');
         $this->configureNavigationIconProperty($property);
+    }
+
+    protected function addPropertiesToClass(ClassType $class): void
+    {
+        parent::addPropertiesToClass($class);
+
+        if ($this->isSimple()) {
+            return;
+        }
+
+        $this->namespace->addUse(SubNavigationPosition::class);
+
+        $class->addProperty('subNavigationPosition', new Literal('SubNavigationPosition::Top'))
+            ->setProtected()
+            ->setStatic()
+            ->setType(SubNavigationPosition::class.'|null');
+    }
+
+    protected function addMethodsToClass(ClassType $class): void
+    {
+        parent::addMethodsToClass($class);
+
+        if ($this->isSimple()) {
+            return;
+        }
+
+        $this->namespace->addUse(Page::class);
+
+        $viewPage = array_key_exists('view', $this->getPageRoutes()) ? $this->getPageRoutes()['view']['class'] : null;
+        $editPage = array_key_exists('edit', $this->getPageRoutes()) ? $this->getPageRoutes()['edit']['class'] : null;
+
+        $this->namespace->addUse($viewPage);
+        $this->namespace->addUse($editPage);
+
+        $methodBody = <<<PHP
+                return \$page->generateNavigationItems([
+                    {$this->simplifyFqn($viewPage)}::class,
+                    {$this->simplifyFqn($editPage)}::class,
+                ]);
+                PHP;
+
+        $method = $class->addMethod('getRecordSubNavigation')
+            ->setPublic()
+            ->setStatic()
+            ->setReturnType('array')
+            ->setBody($methodBody);
+        $method->addParameter('page')
+            ->setType(Page::class);
+
+        $this->configureInfolistMethod($method);
     }
 }
