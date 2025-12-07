b63b777c3ea33b63ee298cb16f80e4a8f28606ce
achyutkneupane <achyutkneupane@gmail.com>
2025-12-07 23:14:02 +0545

    feat: adds SEO page generation for resource management

--- app/Filament/Commands/FileGenerators/Resources/ResourceClassGenerator.php
+++ app/Filament/Commands/FileGenerators/Resources/ResourceClassGenerator.php
@@ -7,6 +7,9 @@ namespace App\Filament\Commands\FileGenerators\Resources;
 use Filament\Commands\FileGenerators\Resources\ResourceClassGenerator as BaseResourceClassGenerator;
 use Filament\Pages\Enums\SubNavigationPosition;
 use Filament\Resources\Pages\Page;
+use Filament\Support\Commands\FileGenerators\Contracts\FileGenerator;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 use Nette\PhpGenerator\ClassType;
 use Nette\PhpGenerator\Literal;
 
@@ -48,6 +51,8 @@ class ResourceClassGenerator extends BaseResourceClassGenerator
     {
         parent::addMethodsToClass($class);
 
+        $this->generateSEOPage();
+
         if ($this->isSimple()) {
             return;
         }
@@ -74,5 +79,33 @@ class ResourceClassGenerator extends BaseResourceClassGenerator
             ->setBody($methodBody);
         $method->addParameter('page')
             ->setType(Page::class);
+    }
+
+    protected function writeFile(string $path, string|FileGenerator $contents): void
+    {
+        $filesystem = app(Filesystem::class);
+
+        $filesystem->ensureDirectoryExists(
+            pathinfo($path, PATHINFO_DIRNAME),
+        );
+
+        $filesystem->put($path, (($contents instanceof FileGenerator) ? $contents->generate() : $contents));
+    }
+
+    private function generateSEOPage(): void
+    {
+        $modelBasename = class_basename($this->modelFqn);
+        $singularModelBasename = Str::singular($modelBasename);
+        $namespace = $this->extractNamespace($this->getFqn());
+        $directory = str_replace('\\', '/', app()->basePath().'/'.str_replace('App\\', 'app/', $namespace));
+
+        $path = "{$directory}/Pages/Manage{$singularModelBasename}SEO.php";
+
+        $this->writeFile($path, app(ResourceSEOPageClassGenerator::class, [
+            'fqn' => "{$namespace}\\Pages\\Manage{$singularModelBasename}SEO",
+            'resourceFqn' => $this->fqn,
+            'hasViewOperation' => $this->hasViewOperation(),
+            'isSoftDeletable' => $this->isSoftDeletable(),
+        ]));
     }
 }
