df5bcdd4e75f19e4a1723b959b8c658dddfe4526
achyutkneupane <achyutkneupane@gmail.com>
2025-12-07 18:19:09 +0545

    feat: updates infolist components generation in SchemasResourceInfolistSchemaClassGenerator

--- app/Filament/Commands/FileGenerators/Resources/SchemasResourceInfolistSchemaClassGenerator.php
+++ app/Filament/Commands/FileGenerators/Resources/SchemasResourceInfolistSchemaClassGenerator.php
@@ -3,22 +3,45 @@
 namespace App\Filament\Commands\FileGenerators\Resources;
 
 use Filament\Commands\FileGenerators\Resources\Schemas\ResourceInfolistSchemaClassGenerator as BaseSchemasResourceInfolistSchemaClassGenerator;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
+use Illuminate\Database\Eloquent\Model;
+use Nette\PhpGenerator\Literal;
 
 class SchemasResourceInfolistSchemaClassGenerator extends BaseSchemasResourceInfolistSchemaClassGenerator
 {
-    public function generateInfolistMethodBody(?string $model = null, array $exceptColumns = []): string
+    /**
+     * @param  ?class-string<Model>  $model
+     * @param  array<string>  $exceptColumns
+     */
+    public function outputInfolistComponents(?string $model = null, array $exceptColumns = []): string
     {
-        return <<<PHP
-            return \$schema
+        $components = $this->getInfolistComponents($model, $exceptColumns);
+        $this->importUnlessPartial(Section::class);
+
+        if (empty($components)) {
+            $recordTitleAttribute = $this->getRecordTitleAttribute();
+
+            if (blank($recordTitleAttribute)) {
+                return '//';
+            }
+
+            $this->importUnlessPartial(TextEntry::class);
+
+            $componentsString = new Literal(<<<PHP
+                {$this->simplifyFqn(TextEntry::class)}::make(?),
+                PHP, [$recordTitleAttribute]);
+        } else {
+            $componentsString = implode(PHP_EOL.'        ', $components);
+        }
+
+        return new Literal(<<<PHP
+            {$this->simplifyFqn(Section::class)}::make()
+                ->columns()
+                ->columnSpanFull()
                 ->components([
-                    Section::make()
-                        ->columns()
-                        ->columnSpanFull()
-                        ->components([
-                            {$this->outputInfolistComponents($model, $exceptColumns)}
-                        ]),
-                ]);
-            PHP;
+                    {$componentsString}
+                ]),
+            PHP);
     }
 }
