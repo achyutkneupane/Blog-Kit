69c3c9308647e10783f98a3f1a82c23663222180
achyutkneupane <achyutkneupane@gmail.com>
2025-12-08 15:49:27 +0545

    feat: adds migration for views table creation

---
+++ database/migrations/2025_12_08_154839_create_views_table.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * The database schema.
+     *
+     * @var Schema
+     */
+    protected $schema;
+
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Create a new migration instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->schema = Schema::connection(
+            config('eloquent-viewable.models.view.connection')
+        );
+
+        $this->table = config('eloquent-viewable.models.view.table_name');
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $this->schema->create($this->table, function (Blueprint $table): void {
+            $table->bigIncrements('id');
+            $table->morphs('viewable');
+            $table->text('visitor')->nullable();
+            $table->string('collection')->nullable();
+            $table->timestamp('viewed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->table);
+    }
+};
