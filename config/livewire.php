29c9f9c1b12e64448d528ea94003c55cae37302e
achyutkneupane <achyutkneupane@gmail.com>
2026-03-10 01:38:22 +0545

    feat: adds color to theme

--- config/livewire.php
+++ config/livewire.php
@@ -32,7 +32,7 @@ return [
     */
 
     'component_namespaces' => [
-        'layouts' => resource_path('views/components/layouts'),
+        'layouts' => resource_path('views/layouts'),
         'pages' => resource_path('views/pages'),
     ],
 
@@ -71,7 +71,7 @@ return [
     */
 
     'make_command' => [
-        'type' => 'class', // Options: 'sfc', 'mfc', 'class'
+        'type' => 'sfc', // Options: 'sfc', 'mfc', 'class'
         'emoji' => true, // Options: true, false
         'with' => [
             'js' => false,
@@ -196,7 +196,7 @@ return [
 
     'navigate' => [
         'show_progress_bar' => true,
-        'progress_bar_color' => '#2299dd',
+        'progress_bar_color' => '#fc6a3e',
     ],
 
     /*
